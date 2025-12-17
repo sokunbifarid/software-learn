0521a865d46ee5213b1cd52b871bbb36769dd514
sokunbi farid <sokunbifarid1@gmail.com>
2025-12-17 17:56:22 +0100

    update

--- blog_app/app.py
+++ blog_app/app.py
@@ -1,1 +1,10 @@
-.
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
