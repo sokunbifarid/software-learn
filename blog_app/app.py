3ec7eb97f2a4b3f37130ae9f9be61133e197d495
sokunbi farid <sokunbifarid1@gmail.com>
2025-12-19 17:54:02 +0100

    update

--- blog_app/app.py
+++ blog_app/app.py
@@ -8,3 +8,35 @@ if __name__ == "__main__":
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+@app.route("/reader/<publisher>/<id>", methods=["GET"])
+def reader(publisher, id):
+    pass
+
+@app.route("/publisher/login", methods=["GET"])
+def publisher_login():
+    pass
+
+@app.route("/publisher/register", methods=["GET", "POST"])
+def publisher_register():
+    pass
+
+@app.route("/publisher/forgot_password", methods=["GET", "POST"])
+def publisher_forgot_password():
+    pass
+
+@app.route("/publisher/recovered_password", methods=["GET"])
+def recovered_password():
+    pass
+
+@app.route("/publisher/edit_post/<id>", methods=["GET","POST", "PUT"])
+def publisher_edit_post():
+    #this section is for showing the post and edit, if the id is null, then
+    #it will load writing a new post and if it is not then it is an edit.
+    pass
+
+@app.route("/publisher/publish_list", methods=["GET"])
+def publisher_post_list():
+    pass
+
+
---
+++ blog_app/static/scripts.js
@@ -0,0 +1,9 @@
+function populate_recent_post_list(value){
+	let holder = document.getElementById("recent-post-holder") 
+	print(value)
+}
+
+function populate_featured_post_list(){
+
+}
+
